3f600fea91dfab1e9bec7f0d6e4ff1e9b0054703
hcz <h.cz@qq.com>
2018-05-27 01:26:20 +0800

    use new proxy interface

--- local.js
+++ local.js
@@ -3,16 +3,73 @@
 const net = require('net');
 
 const socks5 = require('./socks5');
+const proxy = require('./proxy.local');
 
-const createLocal = () => {
+process.on('uncaughtException', (err) => {
+    console.error('global error');
+    console.error(err);
+});
+
+const createLocal = (proxyClient) => {
     return new net.Server({
         allowHalfOpen: true,
     }).on('connection', (socket) => {
         socks5.accept(socket);
 
+        let session = proxyClient.session((data) => {
+            switch (data[0]) {
+                case 'open':
+                    socket.emit('socks5server.open', data[1]);
+
+                    break;
+                case 'connection':
+                    socket.emit('socks5server.connection', data[1]);
+
+                    break;
+                case 'data':
+                    socket.emit('socks5server.data', data[1]);
+
+                    break;
+                case 'end':
+                    socket.emit('socks5server.end');
+
+                    break;
+                case 'close':
+                    socket.emit('socks5server.close');
+
+                    break;
+                default:
+                    // never reach
+                    throw Error();
+            }
+        });
+
         socket.on('error', (err) => {
             console.error('request error');
             console.error(err);
+        }).on('socks5client.connect', (address) => {
+            console.log('connect ' + address);
+
+            proxyClient.send(session, ['connect', address]);
+        }).on('socks5client.bind', (address) => {
+            console.log('bind ' + address);
+
+            proxyClient.send(session, ['bind', address]);
+        }).on('socks5client.udpassociate', (address) => {
+            console.log('udpassociate ' + address);
+
+            proxyClient.send(session, ['udpassociate', address]);
+        }).on('socks5client.data', (chunk) => {
+            proxyClient.send(session, ['data', chunk]);
+        }).on('socks5client.end', () => {
+            proxyClient.send(session, ['end']);
+        }).on('socks5client.close', () => {
+            proxyClient.send(session, ['close']);
+            proxyClient.close(session);
+        }).on('socks5.step', (step) => {
+            console.error('socks5 step ' + step);
+        }).on('socks5.error', (step) => {
+            console.error('socks5 error ' + step);
         });
     }).on('error', (err) => {
         console.error('server error');
@@ -20,9 +77,7 @@ const createLocal = () => {
     });
 };
 
-// module.exports = (options) => {
-//     const self = this;
-// };
-
-// TODO
-createLocal().listen(2333);
+proxy.create((proxyClient) => {
+    // TODO
+    createLocal(proxyClient).listen(2333);
+});
