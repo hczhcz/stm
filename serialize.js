b6701d08b61b203a5d20d76effbaf6dbb575d1a0
hcz <h.cz@qq.com>
2018-05-31 02:01:03 +0800

    modify message serialization

--- serialize.js
+++ serialize.js
@@ -2,34 +2,33 @@
 
 const create = (json, chunk) => {
     const jsonData = Buffer.from(JSON.stringify(json));
-    const sizeData = Buffer.alloc(4);
+    const chunkData = chunk || Buffer.alloc(0);
 
-    sizeData.writeUInt32BE(jsonData.length);
+    const jsonSize = Buffer.alloc(4);
+    const chunkSize = Buffer.alloc(4);
 
-    if (chunk) {
-        return Buffer.concat([
-            sizeData,
-            jsonData,
-            chunk,
-        ]);
-    }
+    jsonSize.writeUInt32BE(jsonData.length);
+    chunkSize.writeUInt32BE(chunkData.length);
 
     return Buffer.concat([
-        sizeData,
+        jsonSize,
+        chunkSize,
         jsonData,
+        chunkData,
     ]);
 };
 
 const getJson = (data) => {
-    const size = data.readUInt32BE();
+    const jsonSize = data.readUInt32BE(0);
 
-    return JSON.parse(data.slice(4, 4 + size).toString());
+    return JSON.parse(data.slice(8, 8 + jsonSize).toString());
 };
 
 const getChunk = (data) => {
-    const size = data.readUInt32BE();
+    const jsonSize = data.readUInt32BE(0);
+    const chunkSize = data.readUInt32BE(4);
 
-    return data.slice(4 + size);
+    return data.slice(8 + jsonSize, 8 + jsonSize + chunkSize);
 };
 
 module.exports = {
