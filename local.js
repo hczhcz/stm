c71726f1d402e7d979124fc829e3ea4b5c0593bd
hcz <h.cz@qq.com>
2018-05-31 02:12:04 +0800

    apply changes

--- local.js
+++ local.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const passSocks5 = require('./pass.socks5')(2333, 2333);
+const passCryptoEncrypt = require('./pass.crypto.encrypt')('fuckGFW');
+const passCryptoDecrypt = require('./pass.crypto.decrypt')('fuckGFW');
+const passCryptoEncrypt2 = require('./pass.crypto.encrypt')('fuckGFW');
+const passCryptoDecrypt2 = require('./pass.crypto.decrypt')('fuckGFW');
 const passProxy = require('./pass.proxy')();
 
 process.on('uncaughtException', (err) => {
@@ -8,5 +12,5 @@ process.on('uncaughtException', (err) => {
     console.error(err);
 });
 
-passSocks5.pipe(passProxy);
-passProxy.pipe(passSocks5);
+passSocks5.pipe(passCryptoEncrypt).pipe(passCryptoDecrypt).pipe(passProxy);
+passProxy.pipe(passCryptoEncrypt2).pipe(passCryptoDecrypt2).pipe(passSocks5);
