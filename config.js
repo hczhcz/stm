8f0a88c653800c3f65062163a5e4b3e2628e4672
hcz <h.cz@qq.com>
2018-06-05 20:54:36 -0400

    use config

---
+++ config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+    local: [
+        ['socks5', 2333],
+        ['zlib.compress', 2],
+        ['crypto.encrypt', 'aes-256-cfb', 'fuckGFW'],
+        ['tcp.client', '127.0.0.1', 2334],
+        ['crypto.decrypt', 'aes-256-cfb', 'fuckGFW2'],
+        ['zlib.decompress', 2],
+        ['segmentation'],
+    ],
+
+    server: [
+        ['tcp.server', 2334],
+        ['crypto.decrypt', 'aes-256-cfb', 'fuckGFW'],
+        ['zlib.decompress', 2],
+        ['segmentation'],
+        ['proxy'],
+        ['zlib.compress', 2],
+        ['crypto.encrypt', 'aes-256-cfb', 'fuckGFW2'],
+    ],
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const config = require('./config');
+
+process.on('uncaughtException', (err) => {
+    console.error('global error');
+    console.error(err);
+});
+
+const configList = config[process.argv[2]];
+const passList = [];
+
+for (let i = 0; i < configList.length; i += 1) {
+    passList.push(require('./pass.' + configList[i][0]).apply(null, configList[i].slice(1)));
+}
+
+passList.push(passList[0]);
+
+for (let i = 1; i < passList.length; i += 1) {
+    passList[i - 1].pipe(passList[i]);
+}
