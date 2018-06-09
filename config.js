fd4f2fbb8ccb6fe4ffb3762de496ae8bbf9f0a59
hcz <h.cz@qq.com>
2018-06-09 00:42:57 -0400

    add pass.http.js

--- config.js
+++ config.js
@@ -11,6 +11,16 @@ module.exports = {
         ['segmentation'],
     ],
 
+    http: [
+        ['http', 2333],
+        ['zlib.compress', 2],
+        ['crypto.encrypt', 'aes-256-cfb', 32, 16, 'fuckGFW'],
+        ['tcp.client', '127.0.0.1', 2334],
+        ['crypto.decrypt', 'aes-256-cfb', 32, 16, 'fuckGFW2'],
+        ['zlib.decompress'],
+        ['segmentation'],
+    ],
+
     server: [
         ['tcp.server', 2334],
         ['crypto.decrypt', 'aes-256-cfb', 32, 16, 'fuckGFW'],
---
+++ pass.http.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const crypto = require('crypto');
+const net = require('net');
+
+const serialize = require('./serialize');
+const httpAgent = require('./http.agent');
+
+module.exports = (listenPort, fullResponse) => {
+    const self = {
+        next: null,
+
+        open: (info, callback) => {
+            callback((data) => {
+                // send
+
+                const json = serialize.getJson(data);
+                const chunk = serialize.getChunk(data);
+
+                switch (json[0]) {
+                    case 'data':
+                        info.socket.emit('httpserver.data', chunk);
+
+                        break;
+                    case 'end':
+                        info.socket.emit('httpserver.end');
+
+                        break;
+                    default:
+                        // ignore
+                }
+            }, () => {
+                // close
+
+                info.socket.emit('httpserver.close');
+            });
+        },
+    };
+
+    net.createServer({
+        allowHalfOpen: true,
+    }).on('connection', (socket) => {
+        socket.pause();
+
+        const id = crypto.randomBytes(2).toString('hex');
+        const info = {
+            socket: socket,
+        };
+
+        self.next(info, (send, close) => {
+            const sendJson = (json, chunk) => {
+                send(serialize.create(json, chunk));
+            };
+
+            httpAgent.accept(socket);
+
+            socket.on('error', (err) => {
+                console.error(id + ' tcp error');
+                console.error(err);
+            }).once('httpclient.request', (address, port) => {
+                console.log(id + ' http request ' + address + ' ' + port);
+
+                sendJson(['connect', address, port], null);
+            }).once('httpclient.connect', (address, port) => {
+                console.log(id + ' http connect ' + address + ' ' + port);
+
+                sendJson(['connect', address, port], null);
+            }).on('httpclient.data', (chunk) => {
+                sendJson(['data'], chunk);
+            }).on('httpclient.end', () => {
+                sendJson(['end'], null);
+            }).on('httpclient.close', () => {
+                close();
+            }).on('http.step', (step) => {
+                // console.error(id + ' http step ' + step);
+            }).on('http.error', (step) => {
+                console.error(id + ' http error ' + step);
+            }).resume();
+        });
+    }).on('error', (err) => {
+        console.error('tcp server error');
+        console.error(err);
+    }).listen(listenPort);
+
+    return self;
+};
