097e5c9dcfc9311a0e71bcbf5873ece653c0b981
hcz <h.cz@qq.com>
2018-07-26 02:11:07 -0400

    split pass.nat.js

--- pass.nat.tcp.js
+++ pass.nat.tcp.js
@@ -2,15 +2,13 @@
 
 const crypto = require('crypto');
 const net = require('net');
-const dgram = require('dgram');
 
 const config = require('./config');
 const serialize = require('./serialize');
 
 module.exports = (
     nextPass /*: Pass */,
-    tcpPolicy /*: Array<[number, string, number]> */,
-    udpPolicy /*: Array<[number, string, number]> */
+    tcpPolicy /*: Array<[number, string, number]> */
 ) /*: Pass */ => {
     const tcpInit = (
         listenPort /*: number */,
@@ -88,87 +86,6 @@ module.exports = (
         tcpInit(tcpPolicy[i][0], tcpPolicy[i][1], tcpPolicy[i][2]);
     }
 
-    const udpInit = (
-        listenPort /*: number */,
-        address /*: string */,
-        port /*: number */
-    ) /*: void */ => {
-        process.nextTick(() /*: void */ => {
-            const info /*: Info */ = {
-                id: crypto.randomBytes(2).toString('hex'),
-            };
-
-            const next /*: BufferGenerator */ = nextPass(info);
-
-            const sendJson = (
-                json /*: Command */,
-                chunk /*: Buffer | null */
-            ) /*: void */ => {
-                next.next(serialize.create(json, chunk));
-            };
-
-            next.next();
-
-            const udpBind /*: dgram$Socket */ = dgram.createSocket({
-                type: 'udp6',
-            }).once('listening', () /*: void */ => {
-                sendJson(['udpassociate'], null);
-            }).on('message', (
-                msg /*: Buffer */,
-                rinfo /*: dgram$Socket$rinfo */
-            ) /*: void */ => {
-                if (
-                    info.udpAddress !== rinfo.address
-                    || info.udpPort !== rinfo.port
-                ) {
-                    console.log(
-                        info.id + ' nat udp ' + address + ' ' + port
-                    );
-
-                    info.udpAddress = rinfo.address;
-                    info.udpPort = rinfo.port;
-                }
-
-                sendJson(['message', address, port], msg);
-            }).once('close', () /*: void */ => {
-                if (config.log.networkClose) {
-                    console.error(info.id + ' nat udpBind close');
-                }
-
-                next.next(null);
-
-                // reconnect
-                // TODO: timeout option?
-                setTimeout(() /*: void */ => {
-                    udpInit(listenPort, address, port);
-                }, 1000);
-            }).on('error', (
-                err /*: error */
-            ) /*: void */ => {
-                if (config.log.networkError) {
-                    console.error(info.id + ' nat udpBind error');
-                }
-
-                if (config.log.networkErrorDetail) {
-                    console.error(err);
-                }
-            });
-
-            // note: not chained according to the official docs
-            udpBind.bind(listenPort);
-
-            info.udpBind = udpBind;
-        });
-    };
-
-    for (
-        let i /*: number */ = 0;
-        i < udpPolicy.length;
-        i += 1
-    ) {
-        udpInit(udpPolicy[i][0], udpPolicy[i][1], udpPolicy[i][2]);
-    }
-
     return function *(
         info /*: Info */
     ) /*: BufferGenerator */ {
@@ -183,20 +100,6 @@ module.exports = (
             const chunk /*: Buffer */ = serialize.getChunk(data);
 
             switch (json[0]) {
-                case 'message':
-                    if (
-                        !info.udpBind
-                        || typeof info.udpAddress !== 'string'
-                        || typeof info.udpPort !== 'number'
-                    ) {
-                        // non-null assertion
-
-                        throw Error();
-                    }
-
-                    info.udpBind.send(chunk, info.udpPort, info.udpAddress);
-
-                    break;
                 case 'data':
                     if (!info.socket) {
                         // non-null assertion
@@ -224,10 +127,6 @@ module.exports = (
 
         if (info.socket) {
             info.socket.destroy();
-        }
-
-        if (info.udpBind) {
-            info.udpBind.close();
         }
     };
 };
---
+++ pass.nat.udp.js
@@ -0,0 +1,131 @@
+'use strict';
+
+const crypto = require('crypto');
+const dgram = require('dgram');
+
+const config = require('./config');
+const serialize = require('./serialize');
+
+module.exports = (
+    nextPass /*: Pass */,
+    udpPolicy /*: Array<[number, string, number]> */
+) /*: Pass */ => {
+    const udpInit = (
+        listenPort /*: number */,
+        address /*: string */,
+        port /*: number */
+    ) /*: void */ => {
+        process.nextTick(() /*: void */ => {
+            const info /*: Info */ = {
+                id: crypto.randomBytes(2).toString('hex'),
+            };
+
+            const next /*: BufferGenerator */ = nextPass(info);
+
+            const sendJson = (
+                json /*: Command */,
+                chunk /*: Buffer | null */
+            ) /*: void */ => {
+                next.next(serialize.create(json, chunk));
+            };
+
+            next.next();
+
+            const udpBind /*: dgram$Socket */ = dgram.createSocket({
+                type: 'udp6',
+            }).once('listening', () /*: void */ => {
+                sendJson(['udpassociate'], null);
+            }).on('message', (
+                msg /*: Buffer */,
+                rinfo /*: dgram$Socket$rinfo */
+            ) /*: void */ => {
+                if (
+                    info.udpAddress !== rinfo.address
+                    || info.udpPort !== rinfo.port
+                ) {
+                    console.log(
+                        info.id + ' nat udp ' + address + ' ' + port
+                    );
+
+                    info.udpAddress = rinfo.address;
+                    info.udpPort = rinfo.port;
+                }
+
+                sendJson(['message', address, port], msg);
+            }).once('close', () /*: void */ => {
+                if (config.log.networkClose) {
+                    console.error(info.id + ' nat udpBind close');
+                }
+
+                next.next(null);
+
+                // reconnect
+                // TODO: timeout option?
+                setTimeout(() /*: void */ => {
+                    udpInit(listenPort, address, port);
+                }, 1000);
+            }).on('error', (
+                err /*: error */
+            ) /*: void */ => {
+                if (config.log.networkError) {
+                    console.error(info.id + ' nat udpBind error');
+                }
+
+                if (config.log.networkErrorDetail) {
+                    console.error(err);
+                }
+            });
+
+            // note: not chained according to the official docs
+            udpBind.bind(listenPort);
+
+            info.udpBind = udpBind;
+        });
+    };
+
+    for (
+        let i /*: number */ = 0;
+        i < udpPolicy.length;
+        i += 1
+    ) {
+        udpInit(udpPolicy[i][0], udpPolicy[i][1], udpPolicy[i][2]);
+    }
+
+    return function *(
+        info /*: Info */
+    ) /*: BufferGenerator */ {
+        // TODO: 2-stage
+
+        for (
+            let data /*: Buffer | null */ = yield;
+            data !== null;
+            data = yield
+        ) {
+            const json /*: Command */ = serialize.getJson(data);
+            const chunk /*: Buffer */ = serialize.getChunk(data);
+
+            switch (json[0]) {
+                case 'message':
+                    if (
+                        !info.udpBind
+                        || typeof info.udpAddress !== 'string'
+                        || typeof info.udpPort !== 'number'
+                    ) {
+                        // non-null assertion
+
+                        throw Error();
+                    }
+
+                    info.udpBind.send(chunk, info.udpPort, info.udpAddress);
+
+                    break;
+                default:
+                    // ignore
+            }
+        }
+
+        if (info.udpBind) {
+            info.udpBind.close();
+        }
+    };
+};
