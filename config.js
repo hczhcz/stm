28c7bd588c9bfe7795e85a107e09727c15004793
hcz <h.cz@qq.com>
2018-07-26 02:11:14 -0400

    update config

--- config.js
+++ config.js
@@ -13,6 +13,12 @@ module.exports = {
             ['segmentation'],
         ],
 
+        _local: [
+            ['_include', '_encode'],
+            ['tcp.client', '-s', '-p'],
+            ['_include', '_decode'],
+        ],
+
         server: [
             ['_description', 'Start remote server'],
             ['tcp.server', '-p'],
@@ -24,25 +30,25 @@ module.exports = {
         socks5: [
             ['_description', 'Start local Socks5 proxy server'],
             ['socks5', '-ls', false],
-            ['_include', '_encode'],
-            ['tcp.client', '-s', '-p'],
-            ['_include', '_decode'],
+            ['_include', '_local'],
         ],
 
         http: [
             ['_description', 'Start local HTTP proxy server'],
             ['http', '-lh', false],
-            ['_include', '_encode'],
-            ['tcp.client', '-s', '-p'],
-            ['_include', '_decode'],
+            ['_include', '_local'],
         ],
 
-        natdemo: [
-            ['_description', 'Start local NAT demo'],
-            ['nat', [[8080, '::1', '-ls']], [[8080, '::1', '-ls']]],
-            ['_include', '_encode'],
-            ['tcp.client', '-s', '-p'],
-            ['_include', '_decode'],
+        tcpnat: [
+            ['_description', 'Start local TCP NAT'],
+            ['nat', [['-nl', '-ns', '-np']]],
+            ['_include', '_local'],
+        ],
+
+        udpnat: [
+            ['_description', 'Start local UDP NAT'],
+            ['nat', [['-nl', '-ns', '-np']]],
+            ['_include', '_local'],
         ],
     },
 
@@ -51,6 +57,9 @@ module.exports = {
         '-p': ['number', 'Port of remote server'],
         '-ls': ['number', 'Port of local Socks5 proxy server'],
         '-lh': ['number', 'Port of local HTTP proxy server'],
+        '-ns': ['string', 'Address of NAT target'],
+        '-np': ['number', 'Port of NAT target'],
+        '-nl': ['number', 'Port of NAT source'],
         '-m': ['string', 'Encrypt method', 'aes-256-cfb'],
         '-k': ['string', 'Encrypt password'],
     },
