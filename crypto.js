2dfe9d010bb81bb121ea7a874f39f55a5d45e5e6
hcz <h.cz@qq.com>
2018-08-23 17:00:46 -0400

    add createKey

--- crypto.js
+++ crypto.js
@@ -10,17 +10,51 @@ const createNonce = (
     return crypto.randomBytes(nonceLength);
 };
 
+const createKey = (
+    algorithm /*: string */,
+    password /*: string */,
+    nonce /*: Buffer */,
+    length /*: number */
+) /*: Buffer */ => {
+    if (algorithm.startsWith('pbkdf2-')) {
+        return crypto.pbkdf2Sync(
+            password,
+            nonce,
+            100000,
+            length,
+            algorithm.slice(7)
+        );
+    }
+
+    if (algorithm === 'scrypt') {
+        return crypto.scryptSync(
+            password,
+            nonce,
+            length
+        );
+    }
+
+    const hmac /*: crypto$Hmac */ = crypto.createHmac(algorithm, nonce);
+
+    hmac.update(password);
+
+    return hmac.digest().slice(0, length);
+};
+
 const createCipher = (
     algorithm /*: string */,
+    kdAlgorithm /*: string */,
     password /*: string */,
     nonce /*: Buffer */
 ) /*: crypto$Cipher */ => {
-    const key /*: Buffer */ = crypto.scryptSync(
+    const key /*: Buffer */ = createKey(
+        kdAlgorithm,
         password + 'key',
         nonce,
         cryptoInfo[algorithm][0] || nonce.length
     );
-    const iv /*: Buffer */ = crypto.scryptSync(
+    const iv /*: Buffer */ = createKey(
+        kdAlgorithm,
         password + 'iv',
         nonce,
         cryptoInfo[algorithm][1] || nonce.length
@@ -31,15 +65,18 @@ const createCipher = (
 
 const createDecipher = (
     algorithm /*: string */,
+    kdAlgorithm /*: string */,
     password /*: string */,
     nonce /*: Buffer */
 ) /*: crypto$Decipher */ => {
-    const key /*: Buffer */ = crypto.scryptSync(
+    const key /*: Buffer */ = createKey(
+        kdAlgorithm,
         password + 'key',
         nonce,
         cryptoInfo[algorithm][0] || nonce.length
     );
-    const iv /*: Buffer */ = crypto.scryptSync(
+    const iv /*: Buffer */ = createKey(
+        kdAlgorithm,
         password + 'iv',
         nonce,
         cryptoInfo[algorithm][1] || nonce.length
@@ -50,10 +87,12 @@ const createDecipher = (
 
 const createHmac = (
     algorithm /*: string */,
+    kdAlgorithm /*: string */,
     password /*: string */,
     nonce /*: Buffer */
 ) /*: () => crypto$Hmac */ => {
-    const key /*: Buffer */ = crypto.scryptSync(
+    const key /*: Buffer */ = createKey(
+        kdAlgorithm,
         password + 'hmac',
         nonce,
         nonce.length
